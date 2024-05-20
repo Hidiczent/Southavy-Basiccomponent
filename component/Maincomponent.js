32c360bd8130469a3b53e590d6984d6383ec02bc
jimmy southavy <you@example.com>
2024-05-20 11:20:41 +0700

    Basiccomponent

---
+++ component/Maincomponent.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View ,Text,Image,StyleSheet} from 'react-native'
+
+function Maincomponent() {
+  return (
+    <View>
+
+            <Text>Southavy Sengdavong</Text>
+            <Image source={{
+                uri:'https://i.pinimg.com/564x/c7/ff/db/c7ffdb5fc01af2e4e71d2a0c90bee61e.jpg'
+            }}/>
+    </View>
+  )
+}
+
+export default Maincomponent
